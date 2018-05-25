410ac4040194187a7509ff411b40334d8c1a0c39
nguyenvantu98 <tuboy.lenam@gmail.com>
2018-05-25 19:58:53 +0700

    testt

--- NguyenTu/TestTu/src/Test.java
+++ NguyenTu/TestTu/src/Test.java
@@ -1,10 +1,9 @@
-package javawork;
 
-public class javamain {
+public class Test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("say goodbye")
+		System.out.println("Test");
 	}
 
 }
