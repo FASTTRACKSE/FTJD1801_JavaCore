97568f64ea27beda6d35dc9c8e9f440f169854bb
Dannydefour <39622221+Dannydefour@users.noreply.github.com>
2018-05-30 19:18:15 +0800

    update for assignment 1 & 2

---
+++ DanialWang/HelloWorld/src/fastrackse/practice1/LineNumber.java
@@ -0,0 +1,11 @@
+package fastrackse.practice1;
+
+public class LineNumber {
+
+	public static void main(String[] args) {
+		for(int i = 0; i>10; i++)
+		System.out.println("This is line number " +i);
+
+	}
+
+}
