4617d17e01e97eec82a2fec538423ec55b51495e
mylien071298 <38049439+mylien071298@users.noreply.github.com>
2018-07-13 20:18:48 +0700

    unit3

--- Lien/JavaFastTrack/src/baitapStudent/SanPham.java
+++ Lien/JavaFastTrack/src/baitapStudent/SanPham.java
@@ -8,9 +8,32 @@ public class SanPham {
 	public double donGia;
 	public double giamGia;
 	public double gia;
+	
+	public SanPham(String name, double donGia, double giamGia) {
+		super();
+		this.name = name;
+		this.donGia = donGia;
+		this.giamGia = giamGia;
+	}
+	
+
+	public SanPham() {
+		super();
+	}
+
+
+	public SanPham(String name, double donGia) {
+		super();
+		this.name = name;
+		this.donGia = donGia;
+		this.giamGia =0;
+	}
+
+
 	public double getThue() {
 		return donGia*(1/10);
 	}
+	
 	public void input() {
 		Scanner sc= new Scanner(System.in);
 		System.out.println("Vui lòng nhập tên sản phẩm: ");
@@ -28,27 +51,39 @@ public class SanPham {
 		// TODO Auto-generated method stub
 		ArrayList<SanPham> list = new ArrayList<SanPham>();
 		SanPham sp = new SanPham();
-		for (int i = 0; i <3 ; i++) {
+		Scanner sc= new Scanner(System.in);
+		System.out.println("Vui lòng nhập tên sản phẩm: ");
+		String name = sc.nextLine();
+		System.out.println("Vui lòng nhập đơn giá: ");
+		Double donGia = Double.parseDouble(sc.nextLine());
+		System.out.println("Vui lòng nhập giảm giá: ");
+		Double giamGia = Double.parseDouble(sc.nextLine());
+		sp.output();
+		SanPham sp1 = new SanPham(name,donGia);
+		sp1.output();
 		
-		sp.input();
-		list.add(sp);
-	
-		
-	}
-		for(int i = 0;i < list.size()-1;i++) {
-			for(int j = i+1; i < list.size();j++) {
-				if(list.get(i).donGia > list.get(j).donGia) {
-					sp.gia = list.get(i).donGia;
-					
-					list.get(i).donGia = list.get(j).donGia;
-					list.get(j).donGia = sp.gia;
-				}
-			}
-			
-		}
-		for(int i = 0 ; i<list.size();i++) {
-			list.get(i).output();
-		}
+//		for (int i = 0; i <3 ; i++) {
+//		
+//		sp.input();
+//		sp1.input();
+//		list.add(sp);
+//	
+//		
+//	}
+//		for(int i = 0;i < list.size()-1;i++) {
+//			for(int j = i+1; i < list.size();j++) {
+//				if(list.get(i).donGia > list.get(j).donGia) {
+//					sp.gia = list.get(i).donGia;
+//					
+//					list.get(i).donGia = list.get(j).donGia;
+//					list.get(j).donGia = sp.gia;
+//				}
+//			}
+//			
+//		}
+//		for(int i = 0 ; i<list.size();i++) {
+//			list.get(i).output();
+//		}
 	}
 
 }
