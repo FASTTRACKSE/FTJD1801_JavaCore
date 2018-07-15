d53a80d07ec4990bad14276ffa2663f67475cb6b
mylien071298 <38049439+mylien071298@users.noreply.github.com>
2018-07-15 18:50:55 +0700

    quanlisinhvien

--- Khanhcodedao/BaiTapCuaFatsTrast/src/MT_1_OOP/sanpham.java
+++ Khanhcodedao/BaiTapCuaFatsTrast/src/MT_1_OOP/sanpham.java
@@ -2,10 +2,54 @@ package MT_1_OOP;
 
 import java.util.Scanner;
 
-public class sanpham {
-	public String tensanpham;
-	public double dongia;
-	public double giamgia;
+public class SanPham {
+	private String tensanpham;
+	private double dongia;
+	private double giamgia = 0;
+	private double getthueThuNhap(double thue) {
+		thue = (dongia/100)*10;
+		return thue;
+	}
+	 
+	public String getTensanpham() {
+		return tensanpham;
+	}
+
+	public void setTensanpham(String tensanpham) {
+		this.tensanpham = tensanpham;
+	}
+
+	public double getDongia() {
+		return dongia;
+	}
+
+	public void setDongia(double dongia) {
+		this.dongia = dongia;
+	}
+
+	public double getGiamgia() {
+		return giamgia;
+	}
+
+	public void setGiamgia(double giamgia) {
+		this.giamgia = giamgia;
+	}
+	
+	
+	public SanPham() {
+		
+	}
+	public SanPham(String tensanpham ,double dongia , double giamgia1) {   // ham tao thu nhat
+		this.tensanpham = tensanpham;
+		this.dongia = dongia;
+		this.giamgia = giamgia;
+	}
+	public SanPham(String tensanpham ,double dongia) {   // ham tao thu 2
+		this.tensanpham = tensanpham;
+		this.dongia = dongia;
+		this.giamgia = 0;
+	}
+	
 	
 	Scanner sc = new Scanner(System.in);
 	public void nhap() {
@@ -13,26 +57,34 @@ public class sanpham {
 		tensanpham = sc.nextLine();
 		System.out.print(" nhap vao don gia : ");
 		dongia = Double.parseDouble(sc.nextLine());
-		System.out.print(" nhap vao giam gia ");
+		System.out.print(" nhap vao giam gia : ");
 		giamgia = Double.parseDouble(sc.nextLine());
-		
 	}
-	public double getThueNhapKhau() {
-		double thue = (dongia/100)*10;
-		return thue;
-	}
-	public void hienThi() {
+	public void xuat() {
 		System.out.println(" ten san pham : "+tensanpham);
 		System.out.println(" don gia : "+dongia);
-		System.out.println(" giam gia : "+giamgia);
-	}
-	public static void main(String[] args) {
-		sanpham sp = new sanpham();
-		sp.nhap();
-		sp.hienThi();
-		double th = sp.getThueNhapKhau();
-		System.out.println(" gia thue la : "+th);
+		if(giamgia != 0) {
+			System.out.println(" giam gia : "+giamgia);
+		}
+		System.out.println(" gia thue la : "+(dongia/100)*10);
 	}
 	
-
+	public static void main(String[] args) {
+		
+		SanPham sp1 = new SanPham();       //  o day van phai nhap vao 3 thong tin tu ban phim
+	    sp1.nhap();                        // 
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.print(" nhap vao ten san pham : ");
+		String tensanpham = sc.nextLine();
+		
+		System.out.print(" nhap vao don gia : ");
+		double dongia = Double.parseDouble(sc.nextLine());
+		
+		SanPham sp2 = new SanPham(tensanpham , dongia);  // o day : gan truc tiep san pham vao mang
+		                                                 // qua do no se nhat biet la ham tao co 2 thuoc tinh 
+		sp1.xuat();
+		sp2.xuat();
+		
+	}
 }
