15c2e716e7deb1f48730c896d147625e27738ab9
trantaitien98 <37853913+trantaitien98@users.noreply.github.com>
2018-07-20 17:43:40 +0700

    Thêm tính năng tìm kiếm V1

--- Tien/StudentManagement/src/student/management/MyWindows.java
+++ Tien/StudentManagement/src/student/management/MyWindows.java
@@ -17,81 +17,70 @@ import java.sql.Statement;
 import java.util.Vector;
 
 import javax.swing.*;
+import javax.swing.border.Border;
+import javax.swing.border.TitledBorder;
 import javax.swing.table.DefaultTableModel;
 
-public class MyWindows extends JFrame{
+public class MyWindows extends JFrame {
 	ResultSet rs = null;
 	Statement stmt;
+
 	public MyWindows() {
 		super("QUAN LY SINH VIEN");
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
-		
+
 		JPanel main = new JPanel();
 		main.setLayout(new BorderLayout());
-		
+
 		JPanel pnMain = new JPanel();
 		pnMain.setLayout(new BorderLayout());
-		
+
 		JPanel pnTop = new JPanel();
 		pnTop.setLayout(new GridLayout(4, 2));
 		pnMain.add(pnTop, BorderLayout.CENTER);
-		
+
 		JLabel lblID = new JLabel("Mã sinh viên: ");
 		pnTop.add(lblID);
 		JTextField txtID = new JTextField(15);
 		pnTop.add(txtID);
-		
+
 		JLabel lblName = new JLabel("Tên sinh viên: ");
 		pnTop.add(lblName);
 		JTextField txtName = new JTextField(15);
 		pnTop.add(txtName);
-		
+
 		JLabel lblGroup = new JLabel("Lớp: ");
 		pnTop.add(lblGroup);
 		JTextField txtGroup = new JTextField(15);
 		pnTop.add(txtGroup);
-		
+
 		try {
 			Class.forName("com.mysql.cj.jdbc.Driver");
-			Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/studentmanagement","root","");
+			Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/studentmanagement", "root", "");
 			stmt = con.createStatement();
-			 rs = stmt.executeQuery("Select * from students");
-			
+			rs = stmt.executeQuery("Select * from students");
+
 		} catch (Exception ex) {
 			System.out.println(ex);
 		}
-		
+
 		JPanel pnBottom = new JPanel();
 		pnMain.add(pnBottom, BorderLayout.SOUTH);
-		
-		Button btnBack = new Button("Back");
+
+		JButton btnBack = new JButton("Back");
 		pnBottom.add(btnBack);
 		btnBack.addActionListener(new ActionClickBack(txtID, txtName, txtGroup, rs));
-		
-		Button btnNext = new Button("Next");
+
+		JButton btnNext = new JButton("Next");
 		pnBottom.add(btnNext);
-		btnNext.addActionListener(new ActionClick(txtID, txtName, txtGroup, rs));		
-		
-		Button btnAdd = new Button("Add");
+		btnNext.addActionListener(new ActionClick(txtID, txtName, txtGroup, rs));
+
+		JButton btnAdd = new JButton("Add");
 		pnBottom.add(btnAdd);
-		btnAdd.addActionListener(new ActionListener() {
-			
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				// TODO Auto-generated method stub
-				try {					
-					stmt.execute("Insert into students(Student_ID,Name,`Group`) values("+txtID.getText()+",\'"+txtName.getText()+"\',\'"+txtGroup.getText()+"\')");
-					JOptionPane.showMessageDialog(null, "Thêm thành công!!!");					
-				} catch (Exception ex) {
-					// TODO: handle exception
-					System.err.println(ex);
-				}
-			}
-		});
-		
-		JPanel tblData = new JPanel();
-		
-		
+
+		JPanel pnData = new JPanel();
+		pnData.setLayout(new BorderLayout());
+
 		DefaultTableModel tblStudent = new DefaultTableModel();
 		tblStudent.addColumn("Mã sinh viên");
 		tblStudent.addColumn("Tên sinh viên");
@@ -108,67 +97,126 @@ public class MyWindows extends JFrame{
 				vec.add(hoTen);
 				vec.add(lop);
 				tblStudent.addRow(vec);
-			} 
+			}
 		} catch (Exception e) {
 			// TODO: handle exception
 		}
-		
-		tbl.addMouseListener( new MouseListener() {
-			
+
+		tbl.addMouseListener(new MouseListener() {
+
 			@Override
 			public void mouseReleased(MouseEvent e) {
 				// TODO Auto-generated method stub
-				int row=tbl.getSelectedRow();
-				
-				String id =(String)tbl.getValueAt(row, 0);
+				int row = tbl.getSelectedRow();
+
+				String id = (String) tbl.getValueAt(row, 0);
 				txtID.setText(id);
-				
-				String name=(String)tbl.getValueAt(row, 1);
+
+				String name = (String) tbl.getValueAt(row, 1);
 				txtName.setText(name);
-				
-				String group =(String)tbl.getValueAt(row, 2);
+
+				String group = (String) tbl.getValueAt(row, 2);
 				txtGroup.setText(group);
-				
+
 			}
-			
+
 			@Override
 			public void mousePressed(MouseEvent e) {
 				// TODO Auto-generated method stub
-				
+
 			}
-			
+
 			@Override
 			public void mouseExited(MouseEvent e) {
 				// TODO Auto-generated method stub
-				
+
 			}
-			
+
 			@Override
 			public void mouseEntered(MouseEvent e) {
 				// TODO Auto-generated method stub
-				
+
 			}
-			
+
 			@Override
 			public void mouseClicked(MouseEvent e) {
 				// TODO Auto-generated method stub
-				
+
 			}
 		});
+
+		btnAdd.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// TODO Auto-generated method stub
+				try {
+					stmt.execute("Insert into students(Student_ID,Name,`Group`) values(" + txtID.getText() + ",\'"
+							+ txtName.getText() + "\',\'" + txtGroup.getText() + "\')");
+					JOptionPane.showMessageDialog(null, "Thêm thành công!!!");
+					tblStudent.setRowCount(0);
+					ResultSet rs = stmt.executeQuery("Select * from students");
+					
+						while (rs.next()) {
+							String maSV = rs.getString(1);
+							String hoTen = rs.getString(2);
+							String lop = rs.getString(3);
+							Vector<String> vec = new Vector<String>();
+							vec.add(maSV);
+							vec.add(hoTen);
+							vec.add(lop);
+							tblStudent.addRow(vec);							
+						}
+						tbl.setModel(tblStudent);
+						tblStudent.fireTableDataChanged();
+					
+				} catch (Exception ex) {
+					// TODO: handle exception
+					System.err.println(ex);
+				}
+			}
+		});
+		JPanel pnSearch = new JPanel();
 		
-		tblData.add(tbl);
+		JLabel lblRow = new JLabel("Tìm kiếm theo: ");
 		
+		JComboBox cbo=new JComboBox(); 
+		cbo.addItem("Mã sinh viên");
+		cbo.addItem("Tên sinh viên");
+		cbo.addItem("Lớp");
+		
+		JLabel lblSearch = new JLabel("Tìm kiếm: ");
+		
+		JTextField txtSearch = new JTextField();
+		
+		JButton btnSearch = new JButton("Tìm kiếm");
+		
+		pnSearch.add(lblRow);
+		pnSearch.add(cbo);
+		pnSearch.add(lblSearch);
+		pnSearch.add(txtSearch);
+		pnSearch.add(btnSearch);
+		
+		pnSearch.setLayout(new GridLayout(1, 5));
+		JPanel pnTable = new JPanel();
+		Border border=  BorderFactory.createLineBorder(Color.RED);
+		TitledBorder borderTitle=  BorderFactory.createTitledBorder(   border, "Danh sách");				
+		pnTable.setBorder(borderTitle);
+		
+		pnTable.add(tbl);
+		pnData.add(pnTable, BorderLayout.SOUTH);
+		pnData.add(pnSearch, BorderLayout.CENTER);
 		main.add(pnMain, BorderLayout.CENTER);
-		main.add(tblData, BorderLayout.SOUTH);
-		
+		main.add(pnData, BorderLayout.SOUTH);
+
 		Container con = getContentPane();
 		con.add(main);
 	}
-	
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		MyWindows mainFrame = new MyWindows();		
-		mainFrame.setSize(400, 300);
+		MyWindows mainFrame = new MyWindows();
+		mainFrame.setSize(600, 300);
 		mainFrame.setLocationRelativeTo(null);
 		mainFrame.setVisible(true);
 	}
